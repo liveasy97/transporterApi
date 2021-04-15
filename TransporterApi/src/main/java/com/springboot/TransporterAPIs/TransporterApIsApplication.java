868b982d07ed7facbaceb1648e3c2c399eb80b7e
chirag4601 <73118852+chirag4601@users.noreply.github.com>
2021-04-15 20:49:29 +0530

    Add files via upload

---
+++ TransporterApi/src/main/java/com/springboot/TransporterAPIs/TransporterApIsApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.TransporterAPIs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporterApIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransporterApIsApplication.class, args);
+		System.out.print("for testing purposes");
+	}
+
+}
